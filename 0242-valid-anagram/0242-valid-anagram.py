491572fa5e5ee624ed6f85f97ee99f33b69eb92a
NJ97761 <128608308+NJ97761@users.noreply.github.com>
2026-01-02 14:51:22 +0530

    Time: 15 ms (42%), Space: 17.6 MB (84.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,29 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
 
-        return True if sorted(s) == sorted(t) else  False
+        if len(s) != len(t):
+            return False
         
+        freq={}
+
+        for char in s:
+            freq[char] = freq.get(char,0)+1
+        for char in t :
+            if char not in freq:
+                return False
+            freq[char] -=1
+            if freq[char] < 0:
+                return False
+        return True
+        
+        # seen = {}
+        # seent = {}
+        
+        # for i in range(len(s)):
+        #     seen[s[i]] = seen.get(s[i],0)+1
+        # for j in range(len(t)):
+        #     seent[t[j]] = seent.get(t[j],0)+1
+        # if seen == seent:
+        #     return True
+        # else:
+        #     return False
