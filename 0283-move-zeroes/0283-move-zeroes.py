66f73327a608b52081aa1122080bce772a2b3698
NJ97761 <128608308+NJ97761@users.noreply.github.com>
2026-01-08 19:22:46 +0530

    Time: 0 ms (100%), Space: 20.6 MB (6.66%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -3,16 +3,15 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        slow = 0
         
-        for fast in range(len(nums)):
-            if nums[fast] != 0:
-                nums[slow] = nums[fast]
-                slow+=1
-        while slow < len(nums):
-            nums[slow] = 0
-            slow+=1
-            
-            
+        left = 0
 
+        for right in range(0,len(nums)):
+            if nums[right] != 0:
+                nums[left] = nums[right]
+                left += 1
+        
+        while left < len(nums):
+                nums[left] = 0
+                left +=1
         
